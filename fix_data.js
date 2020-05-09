9f9c99f7bdc21721e3e4a61b15bd540658766aa5
KhanShaheb34 <shakirul34@student.sust.edu>
2020-05-09 17:11:34 +0600

    Shallow Nural Net

---
+++ fix_data.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+// Test Images
+// let test_images = fs.readFileSync("./dataset/test_images.txt").toString();
+// let test_images_data = [];
+// for (const test of test_images.split("\n"))
+//   test_images_data.push([...test.split(" ")]);
+// console.log(test_images_data.length, test_images_data[434].length);
+// let json_data = JSON.stringify(test_images_data);
+// fs.writeFileSync("./dataset/test_images.json", json_data);
+
+// Train Images
+// let train_images = fs.readFileSync("./dataset/train_images.txt").toString();
+// let train_images_data = [];
+// for (const train of train_images.split("\n"))
+//   train_images_data.push([...train.split(" ")]);
+// console.log(train_images_data.length, train_images_data[434].length);
+// let json_data = JSON.stringify(train_images_data);
+// fs.writeFileSync("./dataset/train_images.json", json_data);
+
+// Test Images
+// let test_labels = fs.readFileSync("./dataset/test_labels.txt").toString();
+// let test_labels_data = test_labels.split(" ");
+// console.log(test_labels_data.length, test_labels_data[434].length);
+// let json_data = JSON.stringify(test_labels_data);
+// fs.writeFileSync("./dataset/test_labels.json", json_data);
+
+// Train Images
+// let train_labels = fs.readFileSync("./dataset/train_labels.txt").toString();
+// let train_labels_data = train_labels.split(" ");
+// console.log(train_labels_data.length, train_labels_data[434].length);
+// let json_data = JSON.stringify(train_labels_data);
+// fs.writeFileSync("./dataset/train_labels.json", json_data);
